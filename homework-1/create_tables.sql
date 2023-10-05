1d7432d9337ea839d486605177295c8eff9db4ac
LudaKend <Ludakend@gmail.com>
2023-10-05 17:49:20 +0300

    hometask 17.1 создание БД, создание таблиц, заполнение таблиц из файлов .csv, просмотр записей в таблицах

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,34 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+    employee_id int PRIMARY KEY,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+    title varchar(100) NOT NULL,
+	birth_date date NOT NULL,
+    notes text
+);
+
+CREATE TABLE customers
+(
+    customer_id char(5) PRIMARY KEY,
+	company_name varchar(50) NOT NULL,
+	contact_name varchar(50) NOT NULL
+);
+
+CREATE TABLE orders
+(
+    order_id char(5) PRIMARY KEY,
+	customer_id char(5) UNIQUE REFERENCES customers(customer_id),
+	employee_id int UNIQUE REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	ship_city varchar(50)
+);
+
+DROP TABLE orders;
+DROP TABLE employees;
+
+SELECT * FROM employees;
+SELECT * FROM customers;
+SELECT * FROM orders;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,63 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import os
+import csv
+
+PASSWORD = os.getenv('FOR_POSTGRES')
+
+#подключаемся к БД
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='180116_Psa'
+)
+
+# создаем курсор
+cur = conn.cursor()
+
+# записываем данные в таблицы
+
+# filename = 'employees_data.csv'
+# path = f'../homework-1/north_data/{filename}'
+# with open(path) as f:
+#     data_file = csv.DictReader(f)
+#     for line in data_file:
+#         cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', (line['employee_id'],
+#                                                                               line['first_name'],
+#                                                                               line['last_name'],
+#                                                                               line['title'],
+#                                                                               line['birth_date'],
+#                                                                               line['notes']))
+#cur.execute('SELECT * FROM employees')
+# conn.commit()
+
+# filename = 'customers_data.csv'
+# path = f'../homework-1/north_data/{filename}'
+# with open(path) as f:
+#     data_file = csv.DictReader(f)
+#     for line in data_file:
+#         cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', (line['customer_id'],
+#                                                                               line['company_name'],
+#                                                                               line['contact_name']))
+# conn.commit()
+
+# filename = 'orders_data.csv'
+# path = f'../homework-1/north_data/{filename}'
+# with open(path) as f:
+#     data_file = csv.DictReader(f)
+#     for line in data_file:
+#         cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', (line['order_id'],
+#                                                                               line['customer_id'],
+#                                                                               line['employee_id'],
+#                                                                        line['order_date'],
+#                                                                        line['ship_city']))
+# conn.commit()
+
+#проверяем записи в таблице
+cur.execute("SELECT *FROM orders")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+cur.close()
