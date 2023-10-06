6695e18f37c9ef27ca344ff948144a154cba3c52
LudaKend <Ludakend@gmail.com>
2023-10-06 20:48:35 +0300

    hometask 17.1 добавила исключения на ошибку записи в таблицу данных с уже имеющимся ключом

--- homework-1/main.py
+++ homework-1/main.py
@@ -17,47 +17,72 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 
 # записываем данные в таблицы
+try:
+    filename = 'employees_data.csv'
+    path = f'../homework-1/north_data/{filename}'
+    with open(path) as f:
+        data_file = csv.DictReader(f)
+        for line in data_file:
+            cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', (line['employee_id'],
+                                                                                  line['first_name'],
+                                                                               line['last_name'],
+                                                                               line['title'],
+                                                                               line['birth_date'],
+                                                                               line['notes']))
+except psycopg2.errors.UniqueViolation:
+    print('запись с таким ключом уже есть в таблице employees')
+else:
+    conn.commit()
 
-# filename = 'employees_data.csv'
-# path = f'../homework-1/north_data/{filename}'
-# with open(path) as f:
-#     data_file = csv.DictReader(f)
-#     for line in data_file:
-#         cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', (line['employee_id'],
-#                                                                               line['first_name'],
-#                                                                               line['last_name'],
-#                                                                               line['title'],
-#                                                                               line['birth_date'],
-#                                                                               line['notes']))
-#cur.execute('SELECT * FROM employees')
-# conn.commit()
+finally:
+    try:
+        filename = 'customers_data.csv'
+        path = f'../homework-1/north_data/{filename}'
+        with open(path) as f:
+            data_file = csv.DictReader(f)
+            for line in data_file:
+                cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', (line['customer_id'],
+                                                                                 line['company_name'],
+                                                                                 line['contact_name']))
+    except psycopg2.errors.InFailedSqlTransaction:
+        print('запись с таким ключом уже есть в таблице customers')
+    else:
+        conn.commit()
+    finally:
+        try:
+            filename = 'orders_data.csv'
+            path = f'../homework-1/north_data/{filename}'
+            with open(path) as f:
+                data_file = csv.DictReader(f)
+                for line in data_file:
+                    cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', (line['order_id'],
+                                                                              line['customer_id'],
+                                                                              line['employee_id'],
+                                                                       line['order_date'],
+                                                                       line['ship_city']))
+        except psycopg2.errors.InFailedSqlTransaction:
+            print('запись с таким ключом уже есть в таблице orders')
+        else:
+            conn.commit()
+        finally:
+            cur.close()
 
-# filename = 'customers_data.csv'
-# path = f'../homework-1/north_data/{filename}'
-# with open(path) as f:
-#     data_file = csv.DictReader(f)
-#     for line in data_file:
-#         cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', (line['customer_id'],
-#                                                                               line['company_name'],
-#                                                                               line['contact_name']))
-# conn.commit()
+# # создаем курсор
+# cur = conn.cursor()
+# # проверяем записи в таблице employees
+# cur.execute("SELECT *FROM employees")
+# rows = cur.fetchall()
+# for row in rows:
+#     print(row)
+# # проверяем записи в таблице customers
+# cur.execute("SELECT *FROM customers")
+# rows = cur.fetchall()
+# for row in rows:
+#     print(row)
+# # проверяем записи в таблице orders
+# cur.execute("SELECT *FROM orders")
+# rows = cur.fetchall()
+# for row in rows:
+#     print(row)
 
-# filename = 'orders_data.csv'
-# path = f'../homework-1/north_data/{filename}'
-# with open(path) as f:
-#     data_file = csv.DictReader(f)
-#     for line in data_file:
-#         cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', (line['order_id'],
-#                                                                               line['customer_id'],
-#                                                                               line['employee_id'],
-#                                                                        line['order_date'],
-#                                                                        line['ship_city']))
-# conn.commit()
 
-#проверяем записи в таблице
-cur.execute("SELECT *FROM orders")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-
-cur.close()
